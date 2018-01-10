17d97241d8f052b0954a3ed9866b218f42613430
Nicko Dela Cruz <nickodc96@gamil.com>
2018-01-09 18:48:38 -0800

    fixed all

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#j-show").slideToggle();
+});
+    $(".clickable1").click(function() {
+    $("#o-show").slideToggle();
+});
+    $(".clickable2").click(function() {
+    $("#v-show").slideToggle();
+});
+    $(".clickable3").click(function() {
+    $("#vnc-show").slideToggle();
+});
+    $(".clickable4").click(function() {
+    $("#f-show").slideToggle();
+});
+    $(".clickable5").click(function() {
+    $("#m-show").slideToggle();
+});
+    $(".clickable6").click(function() {
+    $("#a-show").slideToggle();
+});
+    $(".clickable7").click(function() {
+    $("#p-show").slideToggle();
+});
+    $(".clickable8").click(function() {
+    $("#r-show").slideToggle();
+});
+    $(".clickable9").click(function() {
+    $("#c-show").slideToggle();
+});
+    $(".clickable10").click(function() {
+    $("#s-show").slideToggle();
+});
+    $(".clickable11").click(function() {
+    $("#b-show").slideToggle();
+});
+    $(".clickable12").click(function() {
+    $("#u-show").slideToggle();
+});
+    $(".clickable13").click(function() {
+    $("#n-show").slideToggle();
+});
+    $(".clickable14").click(function() {
+    $("#e-show").slideToggle();
+});
+    $(".theclickable").click(function() {
+    $("#").slideToggle();
+});
+});
